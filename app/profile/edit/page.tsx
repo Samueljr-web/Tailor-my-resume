9033d4b4778e1054876ea63cc185ac3093548a59
Samuel <samueladeyemi2006@gmail.com>
2025-04-15 23:30:21 +0100

    feat: profile edit page

---
+++ app/profile/edit/page.tsx
@@ -0,0 +1,13 @@
+"use client";
+import ProfileEdit from "@/features/profile/profileEdit/profileEdit";
+import React from "react";
+
+function Page() {
+  return (
+    <div>
+      <ProfileEdit />
+    </div>
+  );
+}
+
+export default Page;
